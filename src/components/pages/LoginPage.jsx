3ea50635fd86d809288fcae395aeca6d0e28c9fd
lancemunson <22munsonla@gmail.com>
2025-12-07 16:45:38 -0600

    build bundle

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import TextInput from "../TextInput"
-import { Form } from "react-bootstrap"
+import { Form, Button } from "react-bootstrap"
 export default function LoginPage(props){
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -9,10 +9,12 @@ export default function LoginPage(props){
                                                     display:'flex', 
                                                     justifyContent:'center', 
                                                     alignContent:'center', 
+                                                    alignItems:'center',
                                                     flexDirection:'column'}}>
-                    <h1 className="pageTitle" style={{paddingBottom:50}}>Login</h1>
+                    <h1 className="pageTitle">Login</h1>
                     <TextInput label="Username"/>
-                    <TextInput label="Password"/>
+                    <TextInput label="Password" type="password"/>
+                    <Button className="nonNavButton" style={{marginTop:"1em"}}>Log In</Button> 
                 </Form>
             </>
 }
--- src/components/pages/Registration.jsx
+++ src/components/pages/Registration.jsx
@@ -1,17 +1,29 @@
+import { useRef } from "react"
 import TextInput from "../TextInput"
-import { Form } from "react-bootstrap"
+import { Form, Button } from "react-bootstrap"
 
 export default function Registration(props){
-    return <>
-                <Form className="textInput" style={{width:window.innerWidth, 
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
+    const repeatPasswordRef = useRef(null);
+
+    function register(){
+        alert(usernameRef.current.value)
+    }
+
+    return  <>
+                <Form className="textInput" style={{width:'100%', 
                                                 display:'flex', 
                                                 justifyContent:'center', 
                                                 alignContent:'center', 
+                                                alignItems:'center',
                                                 flexDirection:'column'}}>
+
                     <h1 className="pageTitle">Registration</h1>
-                    <TextInput label="Username"/>
-                    <TextInput label="Password"/>
-                    <TextInput label="Repeat Password"/>
+                    <TextInput label="Username" ref={usernameRef}/>
+                    <TextInput label="Password" type="password" ref={passwordRef}/>
+                    <TextInput label="Repeat Password" type="password" ref={repeatPasswordRef}/>
+                    <Button className="nonNavButton" style={{marginTop:"1em"}} onClick={register}>Register</Button> 
                 </Form>
             </>
 }
